dbf59c48e4ebff111d476457f7cb22177bbb63c9
Karter <kdlmuzixnerd14@gmail.com>
2022-06-29 09:25:13 -0700

    commit tree f82970d0f8c34e076a8f7e6b9fb750078184c70b

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+let cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name)
+{
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name)
+{
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat()
+{
+    return cats.shift();
+}
+
+function destructivelyRemoveLastCat()
+{
+    return cats.pop();
+}
+
+function appendCat(name)
+{
+    const coolCats = [...cats, name];
+    return coolCats;
+}
+
+function prependCat(name)
+{
+    const coolCats = [name, ...cats];
+    return coolCats;
+}
+
+function removeLastCat()
+{
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat()
+{
+    return cats.slice(1);
+}
